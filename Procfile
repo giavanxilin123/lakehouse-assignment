7fb01a1cf11bcaa0bb6206b6a4d36884a3565c3a
giavanxilin123 <1614058@hcmut.edu.vn>
2024-04-03 15:43:37 +0700

    test api

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+pipinstall: pip install -r requirements.txt
--- api.py
+++ api.py
@@ -5,37 +5,41 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # AWS credentials
-AWS_ACCESS_KEY_ID = 'AKIAZSKBNQEVBJS23GFC'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_REGION_NAME = 'ap-northeast-1'
+# AWS_ACCESS_KEY_ID = 'AKIAZSKBNQEVBJS23GFC'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_REGION_NAME = 'ap-northeast-1'
 
-# S3 bucket details
-S3_BUCKET_NAME = 'bucketlakehouse'
-IMAGES_FOLDER = 'output_images'
+# # S3 bucket details
+# S3_BUCKET_NAME = 'bucketlakehouse'
+# IMAGES_FOLDER = 'output_images'
 
-@app.route('/get_image_urls', methods=['GET'])
-def get_image_urls():
-    try:
-        s3_client = boto3.client('s3', 
-                                 aws_access_key_id=AWS_ACCESS_KEY_ID, 
-                                 aws_secret_access_key=AWS_SECRET_ACCESS_KEY, 
-                                 region_name=AWS_REGION_NAME)
+# @app.route('/get_image_urls', methods=['GET'])
+# def get_image_urls():
+#     try:
+#         s3_client = boto3.client('s3', 
+#                                  aws_access_key_id=AWS_ACCESS_KEY_ID, 
+#                                  aws_secret_access_key=AWS_SECRET_ACCESS_KEY, 
+#                                  region_name=AWS_REGION_NAME)
 
-        response = s3_client.list_objects_v2(Bucket=S3_BUCKET_NAME, Prefix=IMAGES_FOLDER)
+#         response = s3_client.list_objects_v2(Bucket=S3_BUCKET_NAME, Prefix=IMAGES_FOLDER)
 
-        image_urls = []
+#         image_urls = []
 
-        for obj in response.get('Contents', []):
-            image_key = obj['Key']
+#         for obj in response.get('Contents', []):
+#             image_key = obj['Key']
 
-            image_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{image_key}"
+#             image_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{image_key}"
 
-            image_urls.append(image_url)
+#             image_urls.append(image_url)
 
-        return jsonify({'image_urls': image_urls}), 200
+#         return jsonify({'image_urls': image_urls}), 200
 
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+#     except Exception as e:
+        # return jsonify({'error': str(e)}), 500
+    
+@app.route('/alo', methods=['GET'])
+def get():
+    return jsonify({'hi': 'hi'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
